89091bf93dd172aa86ab208f4a18b0120beebc1e
coofzilla <jeric614@yahoo.com>
2022-01-28 19:27:18 +0900

    navigator stack setup

---
+++ src/screens/TrackListScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+const TrackListScreen = () => {
+  return (
+    <View>
+      <Text style={{ fontSize: 48 }}>TrackList Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default TrackListScreen;
