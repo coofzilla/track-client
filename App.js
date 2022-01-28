145db010d175e7843a52a044d487d78a4aebd6df
coofzilla <jeric614@yahoo.com>
2022-01-28 20:50:50 +0900

    bottom tab navigator

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
-import * as React from "react";
+import React, { useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import AccountScreen from "./src/screens/AccountScreen";
 import SigninScreen from "./src/screens/SigninScreen";
 import SignupScreen from "./src/screens/SignupScreen";
@@ -10,11 +11,27 @@ import TrackListScreen from "./src/screens/TrackListScreen";
 
 const App = () => {
   const { Navigator, Screen } = createNativeStackNavigator();
+  const Tab = createBottomTabNavigator();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
     <NavigationContainer>
-      <Navigator screenOptions={{ initialRouteName: "Account" }}>
-        <Screen name="Account" component={AccountScreen} />
+      <Navigator screenOptions={{ initialRouteName: "Signin" }}>
+        {isLoggedIn ? (
+          <Tab.Navigator>
+            <Screen name="TrackCreate" component={TrackCreateScreen} />
+            <Screen name="Account" component={AccountScreen} />
+            <Tab.Navigator>
+              <Screen name="TrackDetail" component={TrackDetailScreen} />
+              <Screen name="TrackList" component={TrackListScreen} />
+            </Tab.Navigator>
+          </Tab.Navigator>
+        ) : (
+          <>
+            <Screen name="Signup" component={SignupScreen} />
+            <Screen name="Signin" component={SigninScreen} />
+          </>
+        )}
       </Navigator>
     </NavigationContainer>
   );
