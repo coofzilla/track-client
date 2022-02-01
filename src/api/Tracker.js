cce0396f9972f9af848bf9151fedc42809df1df5
coofzilla <jeric614@yahoo.com>
2022-02-01 09:30:58 +0900

    remove ngrok f/local

--- src/api/Tracker.js
+++ src/api/Tracker.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export default axios.create({
   //change baseURL based on ngrok session
-  baseURL: "http://e556-61-74-229-93.ngrok.io/",
+  baseURL: "http://localhost:3000/",
 });
