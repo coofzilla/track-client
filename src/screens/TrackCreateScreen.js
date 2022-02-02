79154c690d644ca85a76174ec3eec94f8adb17af
coofzilla <jeric614@yahoo.com>
2022-02-02 12:57:09 +0900

    track permissions

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet } from "react-native";
 import { Text } from "react-native-elements";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Map from "../components/Map";
+import { requestForegroundPermissionsAsync } from "expo-location";
 
 const TrackCreateScreen = () => {
+  const [err, setErr] = useState(null);
+
+  const startWatching = async () => {
+    const { granted } = await requestForegroundPermissionsAsync();
+    if (!granted) setErr(true);
+  };
+
+  useEffect(() => {
+    startWatching();
+  }, []);
+
   return (
     <SafeAreaView>
       <Text h2>Create a Track</Text>
       <Map />
+      {err ? <Text>Please enable location services</Text> : null}
     </SafeAreaView>
   );
 };
