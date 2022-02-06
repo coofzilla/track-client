00c9b6150093bd3f9b6d66b312424b05ac4adf85
coofzilla <jeric614@yahoo.com>
2022-02-06 11:21:31 +0900

    send token w/axios f/authentication

--- src/api/Tracker.js
+++ src/api/Tracker.js
@@ -1,6 +1,22 @@
 import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export default axios.create({
+const instance = axios.create({
   //change baseURL based on ngrok session
   baseURL: "http://localhost:3000/",
 });
+
+instance.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (err) => {
+    Promise.reject(err);
+  }
+);
+
+export default instance;
